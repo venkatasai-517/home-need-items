24eafc41d73a361221b27730c22210c5d9ec026c
venkatasai <balanagavenkatasai@gmail.com>
2024-05-07 13:39:22 +0530

    ad's add to html pages

--- TV-washing-Machine/electro-master/js/main.js
+++ TV-washing-Machine/electro-master/js/main.js
@@ -1,3 +1,24 @@
+const toastContainer = document.querySelector(".toast-container");
+const closeBtn = document.querySelector(".toast-container .close");
+const toastLink = document.querySelector(".toast-container a");
+
+if (!localStorage.getItem("displayToast")) {
+  setTimeout(() => {
+    toastContainer.classList.add("active");
+  }, 1000);
+}
+
+const stopDisplayingToast = () => {
+  // localStorage.setItem("displayToast", false);
+  toastContainer.classList.remove("active");
+};
+
+closeBtn.addEventListener("click", stopDisplayingToast);
+toastLink.addEventListener("click", stopDisplayingToast);
+
+
+
+
 (function($) {
 	"use strict"
 
