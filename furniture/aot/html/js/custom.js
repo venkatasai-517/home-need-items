0f66f2fd176d86b9205cfbccb5a3a649121e6e60
venkatasai <balanagavenkatasai@gmail.com>
2024-05-08 14:10:32 +0530

    ad's added to pages

--- furniture/aot/html/js/custom.js
+++ furniture/aot/html/js/custom.js
@@ -1,3 +1,25 @@
+let notifications = document.getElementById('notification');
+let ads = ['https://bvscarpenters.netlify.app',  'https://addtheadd.com','https://bvsadd.netlify.app','https://bvswatchshop.netlify.app']; // Deep link URLs of ads
+let currentIndex = 0;
+
+function displayAd() {
+    notifications.classList.add('show');
+    setTimeout(hideAd, 10000); // Hide ad after 10 seconds
+}
+
+function hideAd() {
+    notifications.classList.remove('show');
+    currentIndex = (currentIndex + 1) % ads.length;
+    setTimeout(displayAd, 10000); // Display next ad after 10 second
+}
+
+function cancelAd() {
+    window.location.href = ads[currentIndex];
+}
+
+displayAd(); // Start displaying ads
+
+
 /*---------------------------------------------------------------------
     File Name: custom.js
 ---------------------------------------------------------------------*/
